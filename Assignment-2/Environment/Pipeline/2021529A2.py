245d4f9dbe3e9c4e57d84714b382442d49ad80c9
divyajeettt <divyajeet21529@iiitd.ac.in>
2024-02-23 20:54:05 +0530

    Added MFCC + Refactor InceptionBlock

--- Assignment-2/Environment/Pipeline/2021529A2.py
+++ Assignment-2/Environment/Pipeline/2021529A2.py
@@ -62,24 +62,17 @@ class AudioDataset(Dataset):
 
         self.datasplit = split
         self.dataset = eval(f"audio_{self.datasplit}_set")
-        self.length = 10000
-        self.sample_rate = 8000
-        self.transform = torchaudio.transforms.MelSpectrogram(sample_rate=self.sample_rate)
+        self.transform = torchaudio.transforms.MFCC(n_mfcc=20, log_mels=True, melkwargs=dict(n_fft=400, hop_length=160, n_mels=40))
 
     def __len__(self) -> int:
         return len(self.dataset)
 
     def __getitem__(self, index: int) -> tuple[torch.Tensor]:
         waveform, sample_rate, label, _, _ = self.dataset[index]
-        resampler = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=self.sample_rate)
-        waveform = resampler(waveform)
-        if waveform.shape[1] < self.length:
-            waveform = nn.functional.pad(waveform, (0, self.length - waveform.shape[1]))
-        elif waveform.shape[1] > self.length:
-            waveform = waveform[:, :self.length]
-        # mel = self.transform(waveform).reshape(51, 128)
-        # return mel, self.MAPPING[label]
-        return waveform, self.MAPPING[label]
+        if waveform.shape[1] < sample_rate:
+            waveform = nn.functional.pad(waveform, (0, sample_rate-waveform.shape[1]))
+        MFCC = self.transform(waveform).squeeze(0)
+        return MFCC, self.MAPPING[label]
 
 
 class ResidualBlock(nn.Module):
@@ -109,7 +102,6 @@ class ResidualBlock(nn.Module):
         out = self.layers(x)
         if out.shape != residual.shape:
             residual = self.residual_conv(residual)
-        # print("residual out:", residual.shape)
         return nn.functional.relu(out + residual)
 
 
@@ -118,52 +110,27 @@ class Resnet_Q1(nn.Module):
                  *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        # # mel spectrograms -> 51 channels of 128 length each
-        # self.layers_1d = nn.Sequential(
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51, stride=2),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51, stride=2),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51, stride=2),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51, stride=2),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51, stride=2),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51, stride=2),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51, stride=2),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51),
-        #     ResidualBlock(dim=1, in_channels=51, out_channels=51),
-        #     nn.Flatten(),
-        #     nn.Linear(51, 35)
-        # )
-
-        # raw audio
         self.layers_1d = nn.Sequential(
-            ResidualBlock(dim=1, in_channels=1, out_channels=2),                  # Block-01:      1 x 10000 ->    2 x 10000
-            ResidualBlock(dim=1, in_channels=2, out_channels=2),                  # Block-02:      2 x 10000 ->    2 x 10000
-            ResidualBlock(dim=1, in_channels=2, out_channels=4, stride=3),        # Block-03:      2 x 10000 ->    4 x  3334
-            ResidualBlock(dim=1, in_channels=4, out_channels=4),                  # Block-04:      4 x  4445 ->    4 x  3334
-            ResidualBlock(dim=1, in_channels=4, out_channels=4, stride=2),        # Block-05:      4 x  3334 ->    8 x  1667
-            ResidualBlock(dim=1, in_channels=4, out_channels=8),                  # Block-06:      8 x  1667 ->    8 x  1667
-            ResidualBlock(dim=1, in_channels=8, out_channels=16, stride=2),       # Block-07:      8 x  1667 ->   16 x   834
-            ResidualBlock(dim=1, in_channels=16, out_channels=16),                # Block-08:     16 x   834 ->   16 x  1334
-            ResidualBlock(dim=1, in_channels=16, out_channels=16, stride=3),      # Block-09:     16 x   834 ->   32 x   278
-            ResidualBlock(dim=1, in_channels=16, out_channels=32),                # Block-10:     32 x   278 ->   32 x   278
-            ResidualBlock(dim=1, in_channels=32, out_channels=32, stride=2),      # Block-11:     32 x   278 ->   64 x   139
-            ResidualBlock(dim=1, in_channels=32, out_channels=64, stride=3),      # Block-12:     64 x   139 ->   64 x    47
-            ResidualBlock(dim=1, in_channels=64, out_channels=64, stride=2),     # Block-13:     64 x    47 ->  128 x    38
-            ResidualBlock(dim=1, in_channels=64, out_channels=128, stride=3),    # Block-14:    128 x    24 ->  128 x     8
-            ResidualBlock(dim=1, in_channels=128, out_channels=128, stride=2),    # Block-15:    128 x     8 ->  256 x     4
-            ResidualBlock(dim=1, in_channels=128, out_channels=256, stride=2),    # Block-16:    256 x     4 ->  256 x     2
-            ResidualBlock(dim=1, in_channels=256, out_channels=256, stride=2),    # Block-17:    256 x     2 ->  512 x     1
-            ResidualBlock(dim=1, in_channels=256, out_channels=256, stride=2),    # Block-18:    512 x     1 ->  512 x     1
+            ResidualBlock(dim=1, in_channels=20, out_channels=20),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20, stride=2),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20, stride=2),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20, stride=2),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20, stride=2),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20, stride=2),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20),
+            ResidualBlock(dim=1, in_channels=20, out_channels=20, stride=2),
             nn.Flatten(),
-            nn.Linear(256, 35)
+            nn.Linear(40, 35)
         )
 
         self.layers_2d = nn.Sequential(
@@ -190,8 +157,7 @@ class Resnet_Q1(nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # return self.layers_1d(x) if x.shape[1] == 51 else self.layers_2d(x)
-        return self.layers_1d(x) if x.shape[1] == 1 else self.layers_2d(x)
+        return self.layers_1d(x) if x.shape[1] == 20 else self.layers_2d(x)
 
 
 class VGGBlock(nn.Module):
@@ -219,38 +185,38 @@ class VGG_Q2(nn.Module):
         super().__init__(*args, **kwargs)
 
         self.layers_1d = nn.Sequential(
-            VGGBlock(dim=1, num_convs=2, in_channels=256, out_channels=256, kernel_size=4, padding=5),
-            VGGBlock(dim=1, num_convs=2, in_channels=256, out_channels=167, kernel_size=5, padding=5),
-            VGGBlock(dim=1, num_convs=3, in_channels=167, out_channels=109, kernel_size=7, padding=5),
-            VGGBlock(dim=1, num_convs=3, in_channels=109, out_channels=71, kernel_size=9, padding=5),
-            VGGBlock(dim=1, num_convs=3, in_channels=71, out_channels=47, kernel_size=12, padding=5),
+            VGGBlock(dim=1, num_convs=2, in_channels=20, out_channels=20, kernel_size=2, padding=1),
+            VGGBlock(dim=1, num_convs=2, in_channels=20, out_channels=13, kernel_size=3, padding=1),
+            VGGBlock(dim=1, num_convs=3, in_channels=13, out_channels=9, kernel_size=4, padding=2),
+            VGGBlock(dim=1, num_convs=3, in_channels=9, out_channels=6, kernel_size=5, padding=3),
+            VGGBlock(dim=1, num_convs=3, in_channels=6, out_channels=4, kernel_size=7, padding=4),
             nn.Flatten(),
             nn.Sequential(
-                nn.Linear(47, 43),
-                nn.Linear(43, 39),
-                nn.Linear(39, 35),
+                nn.Linear(12, 20),
+                nn.Linear(20, 28),
+                nn.Linear(28, 35),
             )
         )
 
         self.layers_2d = nn.Sequential(
-            VGGBlock(dim=2, num_convs=2, in_channels=512, out_channels=512, kernel_size=3, padding=1),
-            VGGBlock(dim=2, num_convs=2, in_channels=512, out_channels=333, kernel_size=4, padding=1),
-            VGGBlock(dim=2, num_convs=3, in_channels=333, out_channels=217, kernel_size=5, padding=2),
-            VGGBlock(dim=2, num_convs=3, in_channels=217, out_channels=142, kernel_size=7, padding=3),
-            VGGBlock(dim=2, num_convs=3, in_channels=142, out_channels=93, kernel_size=9, padding=4),
+            VGGBlock(dim=2, num_convs=2, in_channels=36, out_channels=36, kernel_size=3, padding=1),
+            VGGBlock(dim=2, num_convs=2, in_channels=36, out_channels=24, kernel_size=4, padding=1),
+            VGGBlock(dim=2, num_convs=3, in_channels=24, out_channels=16, kernel_size=5, padding=2),
+            VGGBlock(dim=2, num_convs=3, in_channels=16, out_channels=11, kernel_size=7, padding=3),
+            VGGBlock(dim=2, num_convs=3, in_channels=11, out_channels=8, kernel_size=9, padding=4),
             nn.Flatten(),
             nn.Sequential(
-                nn.Linear(93, 50),
-                nn.Linear(50, 25),
-                nn.Linear(25, 10),
+                nn.Linear(8, 8),
+                nn.Linear(8, 9),
+                nn.Linear(9, 10),
             )
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        if x.shape[1] == 1:
-            return self.layers_1d(x.repeat(1, 256, 1))
+        if x.shape[1] == 20:
+            return self.layers_1d(x)
         else:
-            return self.layers_2d(x.repeat(1, 512//3+1, 1, 1)[:, :-1, :, :])
+            return self.layers_2d(x.repeat(1, 12, 1, 1))
 
 
 class CNA(nn.Module):
@@ -273,33 +239,28 @@ class CNA(nn.Module):
 
 
 class InceptionBlock(nn.Module):
-    def __init__(self, dim: int, channels: int) -> None:
+    def __init__(self, dim: int, in_channels: int, out_1x1: int, out_5x5a: int, out_5x5b: int) -> None:
         super(InceptionBlock, self).__init__()
 
         assert dim == 1 or dim == 2, "dim must be 1 or 2"
 
-        self.branch1 = CNA(dim=dim, in_channels=channels, out_channels=channels, kernel_size=1)
+        self.branch1 = CNA(dim=dim, in_channels=in_channels, out_channels=out_1x1, kernel_size=1)
         self.branch2 = nn.Sequential(
-            CNA(dim=dim, in_channels=channels, out_channels=channels, kernel_size=3, padding=2),
-            CNA(dim=dim, in_channels=channels, out_channels=channels, kernel_size=5, padding=1)
+            CNA(dim=dim, in_channels=in_channels, out_channels=out_5x5a, kernel_size=3, padding=2),
+            CNA(dim=dim, in_channels=out_5x5a, out_channels=out_5x5a, kernel_size=5, padding=1)
         )
         self.branch3 = nn.Sequential(
-            CNA(dim=dim, in_channels=channels, out_channels=channels, kernel_size=3, padding=2),
-            CNA(dim=dim, in_channels=channels, out_channels=channels, kernel_size=5, padding=1)
+            CNA(dim=dim, in_channels=in_channels, out_channels=out_5x5b, kernel_size=3, padding=2),
+            CNA(dim=dim, in_channels=out_5x5b, out_channels=out_5x5b, kernel_size=5, padding=1)
         )
         MaxPool = eval(f"nn.MaxPool{dim}d")
         self.branch4 = MaxPool(3, stride=1, padding=1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # print("x", x.shape)
         branch1 = self.branch1(x)
-        # print("branch1", branch1.shape)
         branch2 = self.branch2(x)
-        # print("branch2", branch2.shape)
         branch3 = self.branch3(x)
-        # print("branch3", branch3.shape)
         branch4 = self.branch4(x)
-        # print("branch4", branch4.shape)
         return torch.cat((branch1, branch2, branch3, branch4), 1)
 
 
@@ -309,25 +270,25 @@ class Inception_Q3(nn.Module):
         super().__init__(*args, **kwargs)
 
         self.layers_1d = nn.Sequential(
-            InceptionBlock(dim=1, channels=1),
-            InceptionBlock(dim=1, channels=1*4),
-            InceptionBlock(dim=1, channels=1*4*4),
-            InceptionBlock(dim=1, channels=1*4*4*4),
+            InceptionBlock(dim=1, in_channels=20, out_1x1=1, out_5x5a=1, out_5x5b=1),
+            InceptionBlock(dim=1, in_channels=23, out_1x1=1, out_5x5a=1, out_5x5b=1),
+            InceptionBlock(dim=1, in_channels=26, out_1x1=1, out_5x5a=1, out_5x5b=2),
+            InceptionBlock(dim=1, in_channels=30, out_1x1=2, out_5x5a=3, out_5x5b=5),
             nn.Flatten(),
-            nn.Linear(1*4*4*4*4*10000, 35)
+            nn.Linear(4040, 35)
         )
 
         self.layers_2d = nn.Sequential(
-            InceptionBlock(dim=2, channels=3),
-            InceptionBlock(dim=2, channels=3*4),
-            InceptionBlock(dim=2, channels=3*4*4),
-            InceptionBlock(dim=2, channels=3*4*4*4),
+            InceptionBlock(dim=2, in_channels=3, out_1x1=1, out_5x5a=2, out_5x5b=2),
+            InceptionBlock(dim=2, in_channels=8, out_1x1=1, out_5x5a=1, out_5x5b=2),
+            InceptionBlock(dim=2, in_channels=12, out_1x1=2, out_5x5a=2, out_5x5b=4),
+            InceptionBlock(dim=2, in_channels=20, out_1x1=2, out_5x5a=3, out_5x5b=5),
             nn.Flatten(),
-            nn.Linear(3*4*4*4*4*32*32, 10)
+            nn.Linear(30720, 10)
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.layers_1d(x) if x.shape[1] == 1 else self.layers_2d(x)
+        return self.layers_1d(x) if x.shape[1] == 20 else self.layers_2d(x)
 
 
 class CustomNetwork_Q4(nn.Module):
@@ -336,37 +297,37 @@ class CustomNetwork_Q4(nn.Module):
         super().__init__(*args, **kwargs)
 
         self.layers_1d = nn.Sequential(
-            ResidualBlock(dim=1, in_channels=1, out_channels=1, stride=3),
-            ResidualBlock(dim=1, in_channels=1, out_channels=1, stride=3),
-            InceptionBlock(dim=1, channels=1),
-            InceptionBlock(dim=1, channels=4),
-            ResidualBlock(dim=1, in_channels=16, out_channels=11, stride=3),
-            InceptionBlock(dim=1, channels=11),
-            ResidualBlock(dim=1, in_channels=44, out_channels=29, stride=3),
-            InceptionBlock(dim=1, channels=29),
-            ResidualBlock(dim=1, in_channels=116, out_channels=76, stride=3),
-            InceptionBlock(dim=1, channels=76),
+            ResidualBlock(dim=1, in_channels=20, out_channels=13, stride=3),
+            ResidualBlock(dim=1, in_channels=13, out_channels=9, stride=3),
+            InceptionBlock(dim=1, in_channels=9, out_1x1=2, out_5x5a=2, out_5x5b=2),
+            InceptionBlock(dim=1, in_channels=15, out_1x1=2, out_5x5a=1, out_5x5b=2),
+            ResidualBlock(dim=1, in_channels=20, out_channels=13, stride=3),
+            InceptionBlock(dim=1, in_channels=13, out_1x1=2, out_5x5a=2, out_5x5b=3),
+            ResidualBlock(dim=1, in_channels=20, out_channels=13, stride=3),
+            InceptionBlock(dim=1, in_channels=13, out_1x1=2, out_5x5a=2, out_5x5b=3),
+            ResidualBlock(dim=1, in_channels=20, out_channels=13, stride=3),
+            InceptionBlock(dim=1, in_channels=13, out_1x1=2, out_5x5a=2, out_5x5b=3),
             nn.Flatten(),
-            nn.Linear(12768, 35)
+            nn.Linear(20, 35)
         )
 
         self.layers_2d = nn.Sequential(
             ResidualBlock(dim=2, in_channels=3, out_channels=2, stride=2),
             ResidualBlock(dim=2, in_channels=2, out_channels=2, stride=2),
-            InceptionBlock(dim=2, channels=2),
-            InceptionBlock(dim=2, channels=8),
-            ResidualBlock(dim=2, in_channels=32, out_channels=21, stride=2),
-            InceptionBlock(dim=2, channels=21),
-            ResidualBlock(dim=2, in_channels=84, out_channels=55, stride=2),
-            InceptionBlock(dim=2, channels=55),
-            ResidualBlock(dim=2, in_channels=220, out_channels=143, stride=2),
-            InceptionBlock(dim=2, channels=143),
+            InceptionBlock(dim=2, in_channels=2, out_1x1=3, out_5x5a=2, out_5x5b=3),
+            InceptionBlock(dim=2, in_channels=10, out_1x1=2, out_5x5a=1, out_5x5b=2),
+            ResidualBlock(dim=2, in_channels=15, out_channels=10, stride=2),
+            InceptionBlock(dim=2, in_channels=10, out_1x1=2, out_5x5a=1, out_5x5b=2),
+            ResidualBlock(dim=2, in_channels=15, out_channels=10, stride=2),
+            InceptionBlock(dim=2, in_channels=10, out_1x1=2, out_5x5a=1, out_5x5b=2),
+            ResidualBlock(dim=2, in_channels=15, out_channels=10, stride=2),
+            InceptionBlock(dim=2, in_channels=10, out_1x1=2, out_5x5a=1, out_5x5b=2),
             nn.Flatten(),
-            nn.Linear(572, 10)
+            nn.Linear(15, 10)
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.layers_1d(x) if x.shape[1] == 1 else self.layers_2d(x)
+        return self.layers_1d(x) if x.shape[1] == 20 else self.layers_2d(x)
 
 
 def trainer(gpu="F",
